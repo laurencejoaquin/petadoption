fd7ea626bb43ea22cdec71a08ea0d74662dd28fc
dandrecollera <dandrecollera@gmail.com>
2024-03-19 22:00:17 +0800

    nav fix

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,33 +1,35 @@
 <nav class="navbar navbar-expand-lg fixed-top border-end" style="background-color: var(--white); border-bottom: 1px solid #ebebeb;">
-    <div class="container-fluid">
+    <div class="container-fluid w-100 d-flex justify-content-between align-items-center">
         <a class="navbar-brand" href="#">
             <img class="main-logo" src="{{ asset('img/PawLogoBeige.png') }}" alt="Paw" height="40" width="40">
             <span class="gochi-hand-regular" style="font-size: 25px;">
                 Paw Buddies
             </span>
         </a>
-        <button class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
+        <button class="navbar-toggler d-lg-none" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
-                {{-- <li class="nav-item">
-                    <a class="nav-link active" href="#" aria-current="page">Home</a>
-                </li> --}}
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"></a>
+                    <a class="nav-link active" href="#" aria-current="page">Home</a>
+                </li>
+                <!-- Add more navigation items here -->
+                <li class="nav-item d-lg-none">
+                    <div class="row gx-2">
+                        <div class="col">
+                            <button class="btn von-login d-block" type="button">Log In</button>
+                        </div>
+                        <div class="col">
+                            <button class="btn von-signup d-block" type="submit">Sign Up</button>
+                        </div>
+                    </div>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                {{-- <button class="btn vonton" type="submit">Login</button> --}}
-                {{-- <a class="btn" data-bs-toggle="button" href="#" role="button">Login</a> --}}
-                <button class="btn von-login" type="button">Log In</button>
-                {{-- <button class="btn btn-link" type="button">Log In</button> --}}
-            </form>
-            <form class="d-flex ms-1" role="search">
-                <button class="btn von-signup" type="submit">Sign Up</button>
-            </form>
-
+        </div>
+        <div class="d-none d-lg-block"> <!-- Hide the buttons on mobile screens -->
+            <button class="btn von-login" type="button">Log In</button>
+            <button class="btn von-signup" type="submit">Sign Up</button>
         </div>
     </div>
 </nav>
